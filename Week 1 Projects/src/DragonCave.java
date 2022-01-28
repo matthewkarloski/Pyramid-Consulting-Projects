c6714cc32e0459b6ce0fc5f5e84a1d3738e45d58
Matthew Karloski <matthew.karloski@yahoo.com>
2022-01-28 08:14:43 -0600

    Week 1 GenSpark Projects from GitLab
    Transferred my code from my GitLab repository to my Github repository

---
+++ Week 1 Projects/src/DragonCave.java
@@ -0,0 +1,87 @@
+import java.util.Random;
+import java.util.Scanner;
+
+//By Matthew Karloski
+//1/26/2022
+//Dragon Cave
+//Current bug if someone doesn't enter in a 1 or 2
+
+public class DragonCave {
+    public static void main(String[] args) throws Exception {
+        /*In this game, the player is in a land full of dragons. The dragons all live in caves
+         * with their large piles o collected treasure. Some dragons are friendly and share their
+         * treasure. Other dragons are hungry and eat anyone who enters their cave. The player
+         * approaches 2 caves, 1 with a friendly dragon and the other with a hungry dragon, but
+         * doesn't know which dragon is in which cave. The player must choose between the two.
+         */
+        dragonCave();
+    }
+
+    public static void dragonCave() throws InterruptedException{
+    	//We will give some story
+        //next we'll need to get input from the user
+        //take the input, and decide whether they get eaten or not
+    	Scanner sc = new Scanner(System.in);
+
+        System.out.println("You are in a land full of dragons. In front of you,");
+        Thread.sleep(2000);
+        System.out.println("you see two caves. In one cave, the dragon is friendly");
+        Thread.sleep(2000);
+        System.out.println("and will share his treasure with you. The other dragon");
+        Thread.sleep(2000);
+        System.out.println("is greedy and hungry and will eat you on sight");
+        Thread.sleep(2000);
+        
+        //Get the input from the user, ensure that they are giving either a 1 or a 2, otherwise tell them to input again
+        int dec;
+        do {
+            System.out.println("Which cave will you go into? (1 or 2)");
+			while (!sc.hasNextInt()) {
+				System.out.println("Invalid input, please enter '1' or '2'.");
+				sc.next();
+			}
+			dec = sc.nextInt();
+			if (dec == 1 || dec == 2) {
+				break;
+			}
+		} while (dec != 1 || dec != 2);
+        System.out.println();
+        
+        //make the hungry dragon random number between 1 and 2, so the user can never guess the 
+        //friendly dragon dragon even after multiple attempts
+        int hunDragon = (int)(Math.random()*2)+1;
+        if (hunDragon == dec) {
+        	//eats player
+        	System.out.println("You approach the cave...");
+        	Thread.sleep(2000);
+        	System.out.println("It is dark and spooky...");
+        	Thread.sleep(2000);
+        	System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+        	Thread.sleep(2000);
+        	System.out.println("Gobbles you down in one bite");
+        }else {
+        	//doesn't eat player
+        	System.out.println("You approach the cave...");
+        	Thread.sleep(2000);
+        	System.out.println("It is dark and spooky...");
+        	Thread.sleep(2000);
+        	System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+        	Thread.sleep(2000);
+        	System.out.println("says 'Hello there old friend!' and gives you a piece of gold from his treasure!");
+        	}
+        //ask if the user wishes to play again
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to play again? (y or n)");
+		
+		while (!scanner.hasNext("[yn]")) { //This line ensures that the user inputed y or n, if not, it'll continue to ask for the right input
+			System.out.println("Not valid input, please input 'y' or 'n'");
+			scanner.next();
+		}
+		
+		String answer = scanner.next();
+		if (answer.equals("y")) dragonCave();
+		else System.out.println("Game Over");
+		sc.close();
+		scanner.close();
+    }
+}
---
+++ Week 1 Projects/src/GuesstheNumber.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+/* Guess the number
+ * By Matthew Karloski
+ * 1/27/2022
+ */
+
+public class GuesstheNumber {
+	/* The computer will pick a random number between 1 to 20 and
+	 * ask the user to guess it. After each guess, the computer will
+	 * tell the user whether the number is too high or too low. User
+	 * wins if they can guess the number within 6 tries
+	 */
+
+	public static void main(String[] args) {
+		//Start the welcome, get their name
+		System.out.println("Hello! What is your name?");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		numberGuess(name);
+		sc.close();
+		System.out.print("Game over");
+		
+	}
+	
+	public static void numberGuess(String name) {
+		System.out.println(String.format("Well %s, I am thinking of a number between 1 and 20", name));
+		//get a random number between 1 and 20
+		int solution = (int)(Math.random()*20)+1;
+		boolean usercorrect = false;
+		Scanner sc2 = new Scanner(System.in);
+		
+		//user gets 6 tries to guess the right number
+		for (int i = 0; i<6; i++) {
+			int userguess;
+			
+			do {
+				System.out.println("Take a guess between 1 and 20.");
+				while (!sc2.hasNextInt()) {
+					System.out.println("Invalid input, please enter a number between 1 and 20");
+					sc2.next();
+				}
+				userguess = sc2.nextInt();
+			} while (userguess <1 || userguess >20);
+			
+			
+			if (userguess == solution) {
+				//User guessed correctly
+				//# of tries is i+1
+				System.out.println(String.format("Good job, %s! You guessed my number in %d", name, i+1));
+				usercorrect = true; //put usercorrect as true
+				break; //get out of the for loop
+			}else {
+				//user was wrong, needs to check whether it's to high or low
+				if(userguess > solution) System.out.println("Your guess is too high.");
+				else System.out.println("Your guess is too low.");
+			}
+		}
+		//check to see if user passed, if not, then tell them that they failed
+		if (!usercorrect) System.out.println(String.format("Sorry, you didn't guess the right number. The solution was %d", solution));
+		
+		
+		//seeing if the user would like to start a new game
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Would you like to play again? (y or n)");
+		
+		while (!scanner.hasNext("[yn]")) { //This line ensures that the user inputed y or n, if not, it'll continue to ask for the right input
+			System.out.println("Not valid input, please input 'y' or 'n'");
+			scanner.next();
+		}
+		
+		String answer = scanner.next();
+		if (answer.equals("y")) numberGuess(name);
+		sc2.close();
+		scanner.close();
+	}
+}
